893c960f67f41e9b697e7ab179919901269bfee4
fkaaziebu <66077189+fkaaziebu@users.noreply.github.com>
2022-10-10 16:18:51 +0000

    First created running web app

---
+++ flaskr/__init__.py
@@ -0,0 +1,39 @@
+"""
+This init file serves double duty
+1. Will contain the application factory
+2. Tells python that the flask directory should be treated as a package
+"""
+
+import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    #create and configure the app
+    """
+    create_app is the application factory function.
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+    
+    if test_config is None:
+        #load the instance config if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        #load the test config if passed in
+        app.config.from_mapping(testing_config)
+    
+    #ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World'
+    return app
